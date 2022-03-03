5b04bec02eab08484cd48997ffcc3370618d3cd8
zzoyu <zzoyu@hotmail.com>
2022-03-04 01:38:49 +0900

    feat(UI): v-model처리

--- vue-drum-pad/src/components/BaseButtonNote.vue
+++ vue-drum-pad/src/components/BaseButtonNote.vue
@@ -12,17 +12,29 @@ const click = () => {
 };
 </script>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  model: {
-    prop: "value",
-    event: "change",
-  },
-});
-</script>
-
 <template>
-  <div class="note" @click="click"></div>
+  <div class="note" @click="click" :class="{ pressed: props.modelValue }"></div>
 </template>
+
+<style scoped>
+.note:nth-of-type(8n + 2),
+.note:nth-of-type(8n + 3),
+.note:nth-of-type(8n + 4),
+.note:nth-of-type(8n + 5) {
+  background-color: rgba(192, 192, 192, 0.6);
+}
+
+.note:nth-of-type(8n + 6),
+.note:nth-of-type(8n + 7),
+.note:nth-of-type(8n + 8),
+.note:nth-of-type(8n + 9) {
+  background-color: rgba(176, 196, 222, 0.6);
+}
+.note.playing {
+  box-shadow: blue 0px 0px 5px;
+}
+.note.pressed {
+  border: darkblue -1px solid;
+  background-color: blue;
+}
+</style>
